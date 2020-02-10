9e8a83d4c5ac39d2e8a63a5c609f65a3930989c7
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2020-02-10 10:25:51 +0530

    Create oopsdemo.py

---
+++ oopsdemo.py
@@ -0,0 +1,13 @@
+# oops demo
+
+class calculator:
+    num = 98
+
+    def getdata(self):
+        print("i am now executing the method in class")
+
+obj = calculator()
+obj.getdata()
+print(obj.num)
+
+
