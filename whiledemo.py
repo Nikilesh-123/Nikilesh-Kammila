96fa25d0a540c1f4ca5a230d745f8f5fdefb431b
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2020-02-10 10:25:06 +0530

    Create whiledemo.py

---
+++ whiledemo.py
@@ -0,0 +1,29 @@
+# while
+
+company = 20
+
+while company >1:
+    print(company)
+    company = company - 1
+
+print("**************************************")
+
+simulation = 1
+for i in range(1, 5, 2):
+    print(simulation)
+    simulation = simulation + i
+
+def gentleman( name ):
+    print("good morning"+ name)
+
+
+gentleman(" nikilesh kammila ")
+
+def addintegers(a, b):
+    print(a+b)
+
+addintegers(15, 23)
+
+
+
+
