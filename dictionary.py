d0b3ba82c750eccdd99aba0c20b9c484f02e8b40
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2020-02-10 10:23:27 +0530

    General code

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+# dictionary
+dic ={"a":"nikilesh", 4: "happy birthday", "c+d": 45}
+
+print(dic["c+d"])
+print(dic)
+print(dic)
+
+
+# new dict
+dict={}
+
+dict["firstname"]= "nikilesh"
+dict["lastname"]= "kammila"
+dict["gender"]= "male"
+dict["age"]= 23
+print(dict)
+print(dict)
+
+
