b0ee90a751fd35d245789b1009f400facefe13b9
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2020-02-10 10:21:47 +0530

    Add files via upload

---
+++ listing.py
@@ -0,0 +1,15 @@
+print(values[0])print(values)
+values = [ "ravi", "rahul", "bindu", "rani" ]
+print(values)
+
+
+values.insert(5, "amith")
+print(values)
+values.insert(6, "suzi")
+print(values)
+del values[2]
+values[-1] = "end of the file"
+print(values)
+values.insert(4, "suzi")
+print(values)
+
