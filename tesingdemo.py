9dc127b8edcf732fa3cd32a63a4433b4c7aabadd
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2020-02-10 10:24:21 +0530

    Create tesingdemo.py

---
+++ tesingdemo.py
@@ -0,0 +1,16 @@
+#python testing
+print("hello nikilesh")
+
+a=2
+print(a)
+
+str="function"
+print(str)
+
+a, b, c = 45, 56.6, "happy"
+print("{} {}".format("add a+b", a+b))
+
+print(type)
+print(type(c))
+print(type(str))
+print(type(a+b))
