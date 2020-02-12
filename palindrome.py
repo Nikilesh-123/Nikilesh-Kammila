5715523b2fb4519504f329069da84002301ae54b
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2020-02-12 21:15:43 +0530

    palindrome str_name[start:end:step]

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+string = input("enter the value of str:")
+rev_string= string[::-1]
+print("reversed string:", rev_string)
+if string == rev_string:
+    print("str is a palindrome:")
+else:
+    print("str is not a palindrome:")
+
+print("*******************************")
+
