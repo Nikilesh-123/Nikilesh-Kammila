c96b671bf6a45f6a9a76b35cbc8557bbb96881d5
Nikilesh-123 <60836456+Nikilesh-123@users.noreply.github.com>
2022-10-25 21:32:29 -0400

    Update facebook.java

--- facebook.java
+++ facebook.java
@@ -11,7 +11,6 @@ public class SampleTest {
 	public static void main(String[] args) {
 		System.out.println("starting sampletest");
 		
-		
 		System.getProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
